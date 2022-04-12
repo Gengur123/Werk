bd05fbd0327f85e5470fb42c569c617708114530
Gengur123 <meleearthur@gmail.com>
2022-04-12 16:18:08 -0700

    Add background color to the image and add slogan

--- src/components/home/Welcome.js
+++ src/components/home/Welcome.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import './Welcome.css'
-// <img src="/image.jpg" alt="image" />
+import Button from '@mui/material/Button';
+
 function Welcome() {
     return (
         <div>
           <div>
-            <h1 className="title-text">DEVIL'S WERK</h1>
+          <br/>
+          <br/>
+            <div className="title-text">DEVIL'S WERK</div>
+            <br/>
+            <br/>
             <div className="column-layout">
-              <span className="header-text">CHECK OUT OUR</span>
+              <span className="header-text">WE DON'T PLAY WITH WHEELS<br/> WE MAKE THEM <span className="red">WERK </span> FOR YOU</span>
+              <div className='title-texts'>CHECK OUT OUR</div>
               <button className="header-button"><a href="#">Inventory</a></button>
             </div>
           </div>
