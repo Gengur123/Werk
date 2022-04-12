fbf0f43d5a2f7bcf6f1d9a02651a69f41a64077b
Gengur123 <meleearthur@gmail.com>
2022-04-11 22:54:44 -0700

    Fix background photo

--- src/App.js
+++ src/App.js
@@ -9,25 +9,25 @@ function App() {
 
   // App.js is gonna be where all our pages will be called and routed. 
   return (
-      <Router>
-        {/* <ul>
-          <li>
-            <Link to='/about'>about</Link>
-          </li>
-          <li>
-            <Link to='/contact'>Contact</Link>
-          </li>
-          <li>
-            <Link to='/'>Home</Link>
-          </li>
-        </ul> */}
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/contact' element={<Contact/>} />
-          <Route path='/about' element={<About />} />
-        </Routes>
-      </Router>
+        <Router>
+          {/* <ul>
+            <li>
+              <Link to='/about'>about</Link>
+            </li>
+            <li>
+              <Link to='/contact'>Contact</Link>
+            </li>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+          </ul> */}
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/contact' element={<Contact/>} />
+            <Route path='/about' element={<About />} />
+          </Routes>
+        </Router>
   );
 }
 
