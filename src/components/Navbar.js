62891aa4cf0f841ebec97357f773fd361fdb2565
Gengur123 <meleearthur@gmail.com>
2022-04-16 14:47:07 -0700

    Add service cards

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,10 +8,11 @@ function Navbar() {
       <div className="navbar">
         <Link to='/' className="logo"><img src="http://via.placeholder.com/60x60"/></Link>
         <ul>
+          <li><button><Link to='/'>Home</Link></button></li>
           <li><button><Link to='/inventory'>Inventory</Link></button></li>
           <li><button><Link to='/service'>Service</Link></button></li>
-          <li><button><Link to='/about'>About Us</Link></button></li>
-          <li><button><Link to="/contact">Contact Us</Link></button></li>
+          <li><button><Link to='/about'>About</Link></button></li>
+          <li><button><Link to="/contact">Contact</Link></button></li>
         </ul>
       </div>
     </>
--- src/components/home/Service.js
+++ src/components/home/Service.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import './Service.css'
 
 function Service() {
   return (
     <>
       <div className='title-text'>
-        SERVICE
+        SERVICES
+      </div>
+      <div className='service-row'>
+        <div className='service-column'>
+          <div  className='service-card'>
+            <div className='service-title'>Wheel Resizing</div>
+          </div>
+        </div>
+        <div className='service-column'>
+          <div  className='service-card'> 
+            <div className='service-title'>Repair Work</div>
+          </div>
+        </div>
+        <div className='service-column'>
+          <div className='service-card'> 
+            <div className='service-title'>Machine Work</div>
+          </div>
+        </div>
+        <div className='service-column'>
+          <div className='service-card'>
+            <div className='service-title'>Refinishing</div>
+          </div>
+        </div>
       </div>   
     </>
     )
