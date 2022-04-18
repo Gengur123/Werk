ac32b7739a2c6dc2b5e46b31c781cf535b921f64
Avery Kyle Arenas <kyruken@gmail.com>
2022-04-17 20:51:44 -0700

    add service column and styles

--- src/components/home/Service.js
+++ src/components/home/Service.js
@@ -1,11 +1,24 @@
 import React from 'react';
-
+import './Service.css'
 function Service() {
   return (
     <>
       <div className='title-text'>
         SERVICE
       </div>   
+      <div className='column-cards'>
+        <div className='card'>
+          <img src='https://www.devilswerk.com/wp-content/uploads/brizy/15/assets/images/iW=600&iH=400&oX=32&oY=0&cW=536&cH=400/Equip-Step-Lip-conversion-2.jpg' className='service-img'></img>
+          <h3>Wheel Resizing</h3>
+          <ul className='service-list'>
+            <li>Step Lip Conversion</li>
+            <li>Up/Down Size Width</li>
+          </ul>
+          </div>
+        <div className='card'><h3>Repair Work</h3></div>
+        <div className='card'><h3>Machine Work</h3></div>
+        <div className='card'><h3>Refinishing</h3></div>
+      </div>
     </>
     )
 }
